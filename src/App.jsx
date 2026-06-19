8efda3a8229118ad1b9a6eb7b96edeac5fbe41a8
anoopvr33 <anoopvranoop33@gmail.com>
2026-06-19 11:55:44 +0530

    hero top

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,20 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import AdminDashboard from "./pages/admin";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div className="app">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color="2,110,126"
+        outerAlpha={0.3}
+        innerScale={1}
+        outerScale={2}
+      />
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
